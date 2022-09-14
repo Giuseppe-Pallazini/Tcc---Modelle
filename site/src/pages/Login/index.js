b52f933331a460abd1547c240e7cbc18f281e25f
Giuseppe Pallazini <Pepepepito1883@gmail.com>
2022-09-14 09:44:21 -0300

    Começo Login denovo

--- site/src/pages/Login/index.js
+++ site/src/pages/Login/index.js
@@ -1,36 +1,45 @@
 import './index.scss';
+import Imagem1 from "../../assets/image/logo_transparent.png"
+import Imagem2 from "../../assets/image/image-login.png";
+
 
 function App() {
   return (
     <main className="main-login">
 
-    <section className="login-section1">
+      <section className="login-section1">
 
-    <div className="login-div1">
-      <img className="login-div1-" src="../../../public/assets/images/logo_transparent.png" alt='' />
+        <img className="section1-img-logo" src={Imagem1} alt='' />
+        <div className="section1-img-modelo-div">
+          <img className="section1-img-modelo" src={Imagem2} alt='' />
+        </div>
 
-      <h1> Inicie Sessão </h1>
-
-      <p>* Campos obrigatorios</p>
-    
-  
-    </div>
-    <div>
-      <input type='text' placeholder='Usuário' />
-      <div className="" />
-
-      <input type='text' placeholder='Senha' />
-  
+        <div className="login-div1">
+          <img className="login-div1-" src="../../../public/assets/images/logo_transparent.png" alt='' />
 
 
 
+          <h1> Inicie Sessão </h1>
 
-      <button> Login </button>
+          <p>* Campos obrigatorios</p>
 
-      </div>
-    </section>
 
-      
+        </div>
+        <div>
+          <input type='text' placeholder='Usuário' />
+
+          <input type='text' placeholder='Senha' />
+
+
+
+
+
+          <button> Login </button>
+
+        </div>
+      </section>
+
+
     </main>
   );
 }
