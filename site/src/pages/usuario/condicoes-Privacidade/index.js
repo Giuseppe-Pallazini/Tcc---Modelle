a8f0496a731689adca85a8bd62f098166a04687e
JuanVictor62 <juan10000victor@gmail.com>
2022-09-22 11:48:46 -0300

    Cabecalho user

--- site/src/pages/usuario/condicoes-Privacidade/index.js
+++ site/src/pages/usuario/condicoes-Privacidade/index.js
@@ -1,6 +1,8 @@
 import { useNavigate} from 'react-router-dom'
 import storage from 'local-storage'
 
+import Cabecalho from '../../../components/cabecalhouser/index.js'
+
 import './index.scss';
 import '../../../assets/common/index.scss'
 import {useEffect, useState, useRef} from 'react'
@@ -8,7 +10,7 @@ import {useEffect, useState, useRef} from 'react'
 export default function Index(){
     return(
        <main>
-        
+            <Cabecalho/>
         </main> 
     );
 }
