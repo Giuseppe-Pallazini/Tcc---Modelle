2964f0946faad52673cd1ee38fffba291ae5b060
Victor Gabriel <vg9469187@email.com>
2022-09-22 23:26:59 -0300

    insert into modelo-marca-tamanho

--- DB/dml.sql
+++ DB/dml.sql
@@ -73,3 +73,75 @@ insert into tb_produto_categoria (id_categoria, id_produto)
 			values		(1,1);
 select * from tb_produto_categoria;
 -- ---------------
+
+--Insert modelos
+
+insert into tb_modelo (nm_modelo)
+            values     ('Camisa');
+insert into tb_modelo (nm_modelo)
+            values     ('Calça');
+insert into tb_modelo (nm_modelo)
+            values     ('Bermuda');
+insert into tb_modelo (nm_modelo)
+            values     ('Blusa');
+insert into tb_modelo (nm_modelo)
+            values     ('Blusa');
+insert into tb_modelo (nm_modelo)
+            values     ('Suéter');
+insert into tb_modelo (nm_modelo)
+            values     ('Relogio');
+insert into tb_modelo (nm_modelo)
+            values     ('Polo');
+insert into tb_modelo (nm_modelo)
+            values     ('Moletom');
+insert into tb_modelo (nm_modelo)
+            values     ('Casaco');
+
+
+--Insert Tamanhos
+insert into tb_tamanho (nm_tamnho)
+            values      ('PP');
+insert into tb_tamanho (nm_tamnho)
+            values      ('P');
+insert into tb_tamanho (nm_tamnho)
+            values      ('M');
+insert into tb_tamanho (nm_tamnho)
+            values      ('G');
+insert into tb_tamanho (nm_tamnho)
+            values      ('GG');
+
+
+--insert Marcas
+
+insert into tb_marca (nm_marca)
+            values    ('Gucci');
+
+insert into tb_marca (nm_marca)
+            values   ('Balenciaga');
+
+insert into tb_marca (nm_marca)
+            values    ('Dolce & Gabbana');
+
+insert into tb_marca (nm_marca)
+            values    ('Burberry');
+
+insert into tb_marca (nm_marca)
+            values   ('Morley');
+
+insert into tb_marca (nm_marca)
+            values   ('Kenzo');
+
+insert into tb_marca (nm_marca)
+            values    ('Modelle');
+
+insert into tb_marca (nm_marca)
+            values   ('Zegna');
+
+insert into tb_marca (nm_marca)
+            values   ('Louis Vuitton');
+
+insert into tb_marca (nm_marca)
+            values   ('Prada');
+
+insert into tb_marca (nm_marca)
+            values    ('Versace');
