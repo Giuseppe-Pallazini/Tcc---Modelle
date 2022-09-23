10831fd291f71488e338472d21699bd09ac3db58
Giuseppe Pallazini <Pepeteste1883@gmail.com>
2022-09-22 22:16:30 -0300

    DML E DDL FINALMENTE ZERO

--- DB/dml.sql
+++ DB/dml.sql
@@ -2,13 +2,53 @@ use ModelleDB;
 
 insert into tb_login (ds_email, ds_senha)
 	values('admin','1234');
-    
-select * from tb_login;
+--
 
-select  id_usuario 			id,
-		id_login 			id_login,
+
+select
+		id_login 			id,
 		ds_email			email,
         ds_senha            senha
 from    tb_login
 where   ds_email = 			'admin'
 and 	ds_senha = 			'1234';
+--
+
+select * from tb_produto;
+--
+
+insert into tb_produto (id_modelo, id_tamanho, nm_produto, nm_prod_complemento, vl_preco, ds_composicao, ds_detalhes, vl_juros, vl_parcela, ds_cor, id_marca, id_categoria)
+			values	   	(1,1,'camisa', 'braba', '1200', 'algodao', 'oi', '15', '12', 'azul', 1, 1);
+-- ---------------
+
+insert into tb_marca (nm_marca)
+			values	 	('Versace');
+            
+insert into tb_produto_marca (id_marca, id_produto)
+			values		(1,1);
+            
+-- ---------------
+
+insert into tb_tamanho (nm_tamanho)
+			values	 	('P');
+            
+insert into tb_produto_tamanho (id_tamanho, id_produto)
+			values		(1,1);
+            
+-- ---------------
+
+insert into tb_modelo (nm_modelo)
+			values	 	('Camisa');
+            
+insert into tb_produto_modelo (id_modelo, id_produto)
+			values		(1,1);
+            
+-- ---------------
+
+insert into tb_categoria (nm_categoria)
+			values	 	('Homem');
+            
+insert into tb_produto_categoria (id_categoria, id_produto)
+			values		(1,1);
+            
+-- ---------------
