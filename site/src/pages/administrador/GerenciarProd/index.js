52a31d127480a8c8bff31cc72b5ba224a5d098e7
Victor Gabriel Rodrigues Silva <vg9469187@gmail.com>
2022-09-27 09:42:29 -0300

    andamento gerenciar produtos

--- site/src/pages/administrador/GerenciarProd/index.js
+++ site/src/pages/administrador/GerenciarProd/index.js
@@ -1,15 +1,55 @@
-import { useNavigate} from 'react-router-dom'
-import storage from 'local-storage'
-import CabeçalhoUser from '../../../components/cabecalhouser'
 
+import CabeçalhoAdm from '../../../components/cabecalhoadm'
+import Iconbuscar from '../../../assets/image/icon-buscar.svg'
+import IconSeta from '../../../assets/image/icon-seta.svg'
 import './index.scss';
 import '../../../assets/common/index.scss'
-import {useEffect, useState, useRef} from 'react'
 
 export default function Index(){
     return(
        <main className='main-gerenciarProd' >
-            <CabeçalhoUser />
+            <CabeçalhoAdm />
+            <section>
+                <div className='div-linha-titulos'>
+                    <div className='div-input-buscar'>
+                        <input type='text' className='gerenciarProd-input-buscar' placeholder='Buscar' />
+                        <img src={Iconbuscar} alt='logo' className='logo-buscar' />
+                    </div>
+
+                    <div className='div-titulo-gerenciar'>
+                        <h1 className='gerenciarProd-titulo'> Gerenciar produtos </h1>
+                    </div>
+
+                    <div className='div-titulo-gerenciar'>
+                        <div className='div-nome-mes'>
+                            <p className='gerenciarProd-fevereiro'> Fevereiro</p>
+                            <img src={IconSeta} alt='logo' className='gerenciarProd-logo-buscar' /> 
+                        </div>
+                        <div className='select-mes'>
+                            <input type='text' className='input-mes' placeholder='Mês: ' />
+                        </div>
+                        <div className='select-ano'>
+                            <input type='text' className='input-ano' placeholder='Ano:  ' />
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className='div-linha-cards'>
+                    <div className='gerenciarProd-card-1'>
+
+                    </div>
+
+                    <div className='gerenciarProd-card-2'>
+
+                    </div>
+
+                    <div className='gerenciarProd-card-3'>
+
+                    </div>
+
+                </div>
+            </section>
         </main> 
     );
 }
