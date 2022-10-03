b7f2c1a50ce501fd4cb47b71b8915008f377a8ad
Victor Gabriel <vg9469187@gmail.com>
2022-10-03 09:40:22 -0300

    começo cadastro usuario

---
+++ API/src/controller/cadastroUsuarioController.js
@@ -0,0 +1,18 @@
+import { CadastrarUsuario } from "../repository/cadastroUsuarioRepository.js";
+
+import { Router } from "express";
+const server = Router();
+
+server.post('/usuario/cadastro', async (req, resp) => {
+    try {
+        const novoUsuario = req.body;
+
+        const usuarioInserido = await CadastrarUsuario(novoUsuario);
+        resp.send(usuarioInserido);
+    } catch (err) {
+        resp.status(401).send({
+            erro: err.message
+        });
+    }
+})
+
