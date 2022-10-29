27241a8382dfd400e0261797923b8e4b02a33ad5
JuanVictor62 <juan10000victor@gmail.com>
2022-10-29 09:41:28 -0300

    page gerenciar usuario e feedback

--- site/src/pages/administrador/GerenciarFeedbecks/index.js
+++ site/src/pages/administrador/GerenciarFeedbecks/index.js
@@ -3,13 +3,68 @@ import { useNavigate} from 'react-router-dom'
 import storage from 'local-storage'
 import CabecalhoAdm from '../../../components/cabecalhoadm'
 
+import LogoLupa from '../../../assets/image/logo-lupa.png'
 import '../../../assets/common/index.scss'
 import {useEffect, useState, useRef} from 'react'
 
 export default function Index(){
     return(
-       <main>
+       <main className='main-gerenciarUsuarios'>
             <CabecalhoAdm />
+                <h1 className='gerenciarFeedbacks-h1'>FeedBacks</h1>
+            <section className='section-table'>
+                
+                <div>
+                    <table className='gerenciarUser-table'>
+                        <thead className='gereciarFeed-thead-table'>
+                            <tr className='gereciarFeed-tr-titulo'>
+                                <th>USUARIO</th>
+                                <th>E-MAIL</th>
+                                <th>Mensagem</th>
+                                <th>Pedido</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody className='gereciarFeed-tbody'>
+                            <tr className='gereciarFeed-tr'>
+                                <td className='gereciarFeed-td' >Vitão</td>
+                                <td className='gereciarFeed-td' >Vg9459544@gmail.com</td>                               
+                                <td className='gereciarFeed-td-mensagem' >Atedimento é otimo!!!.....</td>
+                                <td className='gereciarFeed-td' ># 01</td>
+                                <td>
+                                    
+                                    <img src={LogoLupa} alt='logo-lupa' />
+                                </td>
+                            </tr>
+
+
+                            <tr className='gereciarFeed-tr'>
+                                <td className='gereciarFeed-td' >Vitão</td>
+                                <td className='gereciarFeed-td' >Vg9459544@gmail.com</td>                               
+                                <td className='gereciarFeed-td-mensagem' >Atedimento é otimo!!!.....</td>
+                                <td className='gereciarFeed-td' ># 01</td>
+                                <td>
+                                    
+                                    <img src={LogoLupa} alt='logo-lupa' />
+                                </td>
+                            </tr>
+                
+
+                            <tr className='gereciarFeed-tr'>
+                                <td className='gereciarFeed-td' >Vitão</td>
+                                <td className='gereciarFeed-td' >Vg9459544@gmail.com</td>                               
+                                <td className='gereciarFeed-td-mensagem' >Atedimento é otimo!!!.....</td>
+                                <td className='gereciarFeed-td' ># 01</td>
+                                <td>
+                                    
+                                    <img src={LogoLupa} alt='logo-lupa' />
+                                </td>
+                            </tr>
+                
+                        </tbody>
+                    </table>
+                </div>
+            </section>
         </main> 
     );
 }
--- site/src/pages/administrador/GerenciarUsuarios/index.js
+++ site/src/pages/administrador/GerenciarUsuarios/index.js
@@ -3,13 +3,68 @@ import { useNavigate} from 'react-router-dom'
 import storage from 'local-storage'
 import CabecalhoAdm from '../../../components/cabecalhoadm'
 
+import LogoRemover from '../../../assets/image/logo-remover.svg'
+import LogoEditar from '../../../assets/image/logo-editar.svg'
 import '../../../assets/common/index.scss'
 import {useEffect, useState, useRef} from 'react'
 
 export default function Index(){
     return(
-       <main>
+       <main className='main-gerenciarUsuarios'>
             <CabecalhoAdm />
+
+            <h1 className='gerenciarUsuarios-h1'>Usuarios</h1>
+            <section className='section-table'>
+                <div>
+                    <table className='gerenciarUser-table'>
+                        <thead className='gereciarUser-thead-table'>
+                            <tr className='gereciarUser-tr-titulo'>
+                                <th>USUARIO</th>
+                                <th>E-MAIL</th>
+                                <th>Dt. Nascimento</th>
+                                <th>Número</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody className='gereciarUser-tbody'>
+                            <tr className='gereciarUser-tr'>
+                                <td className='gereciarUser-td' >Vitão</td>
+                                <td className='gereciarUser-td' >Vg9459544@gmail.com</td>                               
+                                <td className='gereciarUser-td' >12 / 10 / 2006</td>
+                                <td className='gereciarUser-td' >11971218295</td>
+                                <td>
+                                    <img src={LogoEditar} alt='editar' />
+                                    &nbsp;&nbsp;
+                                    <img src={LogoRemover} alt='remover' />
+                                </td>
+                            </tr>
+                          
+                            <tr className='gereciarUser-tr'>
+                                <td className='gereciarUser-td' >Vitão</td>
+                                <td className='gereciarUser-td' >Vg9459544@gmail.com</td>                               
+                                <td className='gereciarUser-td' >12 / 10 / 2006</td>
+                                <td className='gereciarUser-td' >11971218295</td>
+                                <td>
+                                    <img src={LogoEditar} alt='editar' />
+                                    &nbsp;&nbsp;
+                                    <img src={LogoRemover} alt='remover' />
+                                </td>
+                            </tr>
+                            <tr className='gereciarUser-tr'>
+                                <td className='gereciarUser-td' >Vitão</td>
+                                <td className='gereciarUser-td' >Vg9459544@gmail.com</td>                               
+                                <td className='gereciarUser-td' >12 / 10 / 2006</td>
+                                <td className='gereciarUser-td' >11971218295</td>
+                                <td>
+                                    <img src={LogoEditar} alt='editar' />
+                                    &nbsp;&nbsp;
+                                    <img src={LogoRemover} alt='remover' />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
         </main> 
     );
 }
