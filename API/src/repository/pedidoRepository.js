8e3d2b506dc0d98ec36d3255aa7c43a3e604dcb9
JuanVictor62 <juan10000victor@gmail.com>
2022-11-03 10:52:01 -0300

    03/11

--- API/src/repository/pedidoRepository.js
+++ API/src/repository/pedidoRepository.js
@@ -4,18 +4,41 @@ import { con } from "./connection.js"
 export async function inserirPedido(novoPedido) {
     const comando = `
         INSERT INTO tb_pedido (
-
+            id_usuario,
+            id_cupom,
+            dt_pedido,
+            cod_nota_fiscal,
+            tp_frete,
+            vl_frete,
+            ds_status,
+            tp_pagamento
         )
-
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
     `
+    const [info] = await con.query(comando, [
+        novoPedido.idUsuario,
+        novoPedido.idCupom,
+        novoPedido.data,
+        novoPedido.notaFiscal,
+        novoPedido.tipoFrete,
+        novoPedido.valorFrete,
+        novoPedido.status,
+        novoPedido.tipoPagamento
+    ]);
 }
 
 export async function inserirPagamento(idPedido, novoPagamento) {
     const comando = `
         INSERT INTO tb_pagamento_cartao (
-
+            id_pedido,
+            nm_cartao,
+            nr_cartao,
+            dt_vencimento,
+            cod_seguranca,
+            nr_parcelas,
+            ds_forma_pagmento
         )
-    
+        VALUES (?, ?, ?, ?, ?, ?, ?)
     `
 
     const [info] = await con.query(comando, [
